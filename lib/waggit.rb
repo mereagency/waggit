a051ad4b1b36c4d544970dd19c9e7bec678a8a2d
Jon Grose <jonathanbrose@gmail.com>
2014-09-17 12:13:28 -0500

    add waggit pull

--- lib/waggit.rb
+++ lib/waggit.rb
@@ -36,6 +36,43 @@ module Waggit
     puts Wagon.push(options)
   end
 
+  def self.pull(options)
+    begin
+      puts Git.checkout_master
+      puts Git.delete_wagon
+      puts Git.delete_local
+      puts Git.stash
+
+      puts Git.checkout_new_wagon
+      puts Wagon.pull(options)
+      #TODO: Checkout: http://stackoverflow.com/questions/3515597/git-add-only-non-whitespace-changes
+      if Git.has_changes?
+        puts Git.add_all
+        puts Git.commit "merge wagon pull"
+      end
+
+      puts Git.checkout_master
+      puts Git.rebase_wagon
+      puts Git.checkout_master
+      puts Git.merge_wagon
+
+      puts Git.checkout_new_local
+      puts Git.stash_pop
+      puts Git.add_all
+      puts Git.commit_prompt
+      puts Git.rebase_local
+      puts Git.checkout_master
+      puts Git.merge_local
+      puts Git.delete_wagon
+      puts Git.delete_local
+  	rescue Exception
+      puts "Pull aborted, switching back to master branch"
+      puts Git.checkout_master
+      raise
+    end
+
+  end
+
   def self.sync(options)
     begin
       puts Git.checkout_master
